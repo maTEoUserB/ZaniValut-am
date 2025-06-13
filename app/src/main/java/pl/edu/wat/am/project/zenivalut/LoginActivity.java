9245ea8cc3812225a51a02cd93c5d0b39d4dfe86
Mateusz <mateu@_matbe>
2025-06-13 19:54:51 +0200

    Added new layout and menu toolbar, changed names

---
+++ app/src/main/java/pl/edu/wat/am/project/zenivalut/LoginActivity.java
@@ -0,0 +1,67 @@
+package pl.edu.wat.am.project.zenivalut;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import pl.edu.wat.am.project.zenivalut.model.LoginData;
+import pl.edu.wat.am.project.zenivalut.repository.ApiInstance;
+import pl.edu.wat.am.project.zenivalut.repository.ApiService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        text = findViewById(R.id.text);
+
+//        RegisterData registerData = new RegisterData("newExampleUser", "123", 100.0);
+        LoginData loginData = new LoginData("newExampleUser", "123");
+
+        ApiService apiService = ApiInstance.getInstance().create(ApiService.class);
+        Call<ResponseBody> call = apiService.loginUser(loginData);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String responseString = "";
+                try {
+                    if (response.body() != null) {
+                        responseString = response.body().string();
+                        text.setText(responseString);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Toast.makeText(LoginActivity.this, "Działa!" + responseString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                text.setText(t.getMessage());
+                Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
