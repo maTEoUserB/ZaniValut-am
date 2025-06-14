3f321d21e16dfebfce48d80d53f6665dff5846fc
Mateusz <mateu@_matbe>
2025-06-14 15:19:00 +0200

    Added bottom navigation, logo, bettes style

--- app/src/main/java/pl/edu/wat/am/project/zenivalut/MainActivity.java
+++ app/src/main/java/pl/edu/wat/am/project/zenivalut/MainActivity.java
@@ -4,26 +4,30 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import pl.edu.wat.am.project.zenivalut.repository.ApiInstance;
+import pl.edu.wat.am.project.zenivalut.repository.ApiService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+
+    TextView balanceTextView;
+    TextView euroTextView;
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_logout) {
-            Toast.makeText(this, "Wylogowano", Toast.LENGTH_SHORT).show();
-            // Tutaj możesz dodać Intent do ekranu logowania lub wyczyścić dane sesji
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @SuppressLint("MissingInflatedId")
@@ -32,7 +36,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        balanceTextView = findViewById(R.id.balanceTextView);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        String token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuZXdFeGFtcGxlVXNlciIsImlhdCI6MTc0OTkwNTkyNSwiZXhwIjoxNzQ5OTA5NTI1fQ.GbJNgpSmyXXgxzj0TZmCjuk6aYulupIs9z0DtaezbMM";
+        ApiService apiService = ApiInstance.getInstance().create(ApiService.class);
+        Call<ResponseBody> call = apiService.getBalance(token);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                String responseString = "";
+                if (response.body() != null) {
+                    try {
+                        responseString = response.body().string();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    balanceTextView.setText("SALDO: " + responseString + " PLN");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
