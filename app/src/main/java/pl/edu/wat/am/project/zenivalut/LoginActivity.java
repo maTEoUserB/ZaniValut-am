8a38133ef6f7c7680e263bbb31c8d4b290ca8961
Mateusz <mateu@_matbe>
2025-06-14 16:28:44 +0200

    Login connected to server, added intents and fixed login/register views

--- app/src/main/java/pl/edu/wat/am/project/zenivalut/LoginActivity.java
+++ app/src/main/java/pl/edu/wat/am/project/zenivalut/LoginActivity.java
@@ -1,26 +1,33 @@
 package pl.edu.wat.am.project.zenivalut;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.IOException;
+import com.google.android.material.textfield.TextInputEditText;
+
 
 import okhttp3.ResponseBody;
 import pl.edu.wat.am.project.zenivalut.model.LoginData;
-import pl.edu.wat.am.project.zenivalut.repository.ApiInstance;
-import pl.edu.wat.am.project.zenivalut.repository.ApiService;
+import pl.edu.wat.am.project.zenivalut.repository.AuthRepository;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView text;
+
+    TextInputEditText username;
+    TextInputEditText password;
+    Button loginButton;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,35 +40,45 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        text = findViewById(R.id.text);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.register_button);
 
-//        RegisterData registerData = new RegisterData("newExampleUser", "123", 100.0);
-        LoginData loginData = new LoginData("newExampleUser", "123");
 
-        ApiService apiService = ApiInstance.getInstance().create(ApiService.class);
-        Call<ResponseBody> call = apiService.loginUser(loginData);
+        loginButton.setOnClickListener(v -> {
 
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                String responseString = "";
-                try {
-                    if (response.body() != null) {
-                        responseString = response.body().string();
-                        text.setText(responseString);
+            String login = username.getText().toString().trim();
+            String passwd = password.getText().toString().trim();
+
+            LoginData loginData = new LoginData(login, passwd);
+
+            AuthRepository authRepo = new AuthRepository(this);
+            authRepo.loginUser(loginData, new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if (response.isSuccessful()) {
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Błąd logowania.", Toast.LENGTH_SHORT).show();
                     }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
                 }
-                Toast.makeText(LoginActivity.this, "Działa!" + responseString, Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                text.setText(t.getMessage());
-                Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+
+
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         });
 
     }
+
 }
---
+++ app/src/main/java/pl/edu/wat/am/project/zenivalut/repository/AuthRepository.java
@@ -0,0 +1,54 @@
+package pl.edu.wat.am.project.zenivalut.repository;
+
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import pl.edu.wat.am.project.zenivalut.model.LoginData;
+import pl.edu.wat.am.project.zenivalut.repository.retrofit.ApiInstance;
+import pl.edu.wat.am.project.zenivalut.repository.retrofit.ApiService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AuthRepository {
+    private final Context context;
+
+    public AuthRepository(Context context) {
+        this.context = context;
+    }
+    public void loginUser(LoginData loginData, Callback<ResponseBody> callback){
+        ApiService apiService = ApiInstance.getInstance().create(ApiService.class);
+        Call<ResponseBody> call = apiService.loginUser(loginData);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    try {
+                        String token = response.body().string();
+                        SharedPreferences prefs = context.getSharedPreferences("auth", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("token", token);
+                        editor.apply();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                callback.onResponse(call, response);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                callback.onFailure(call, t);
+            }
+        });
+    }
+}
